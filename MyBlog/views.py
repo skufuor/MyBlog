fc47d7f26eef1c172e9446ed0c673105c6f7b8cf
Stephen Kufuor <kufuo004@gmail.com>
2016-01-15 21:02:38 -0600

    Adding working routing for blog

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
-def main_page(request):
+def home_page(request):
     return HttpResponse("This is my Blog.")
+
+def about_me(request):
+    return HttpResponse("About Me!!!")
+
+def page(request, page_name):
+    output_str = "A specfic page: " + page_name    
+    return HttpResponse(output_str)
+
+def newest_page(request):
+    return HttpResponse("This is my newest blog post")
